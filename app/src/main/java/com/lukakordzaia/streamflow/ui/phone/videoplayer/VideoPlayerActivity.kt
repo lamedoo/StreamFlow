68e31651a7c3bdff9757a93f5cd9218eec5f99f3
lkordzaia <lukak@leavingstone.com>
2021-08-18 16:26:44 +0400

    PHONE - fixed video player stacking twice on each other

--- app/src/main/java/com/lukakordzaia/streamflow/ui/phone/videoplayer/VideoPlayerActivity.kt
+++ app/src/main/java/com/lukakordzaia/streamflow/ui/phone/videoplayer/VideoPlayerActivity.kt
@@ -7,16 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.lukakordzaia.streamflow.R
 import com.lukakordzaia.streamflow.datamodels.VideoPlayerData
 import com.lukakordzaia.streamflow.utils.AppConstants
-import kotlinx.android.synthetic.main.activity_phone_video_player.*
 
 class VideoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_video_player)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.tv_video_player_fragment, VideoPlayerFragment())
-            .commit()
     }
 
     companion object {
