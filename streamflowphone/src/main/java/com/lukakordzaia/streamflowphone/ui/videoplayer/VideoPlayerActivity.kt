d164edf6e20aaa1b82932585f5ee13d66dfa9285
Luka Kordzaia <lkordzaia@tbcbank.com.ge>
2022-06-06 15:54:16 +0400

    PHONE: Fixed status bar not hiding on vieo player

--- streamflowphone/src/main/java/com/lukakordzaia/streamflowphone/ui/videoplayer/VideoPlayerActivity.kt
+++ streamflowphone/src/main/java/com/lukakordzaia/streamflowphone/ui/videoplayer/VideoPlayerActivity.kt
@@ -21,7 +21,7 @@ class VideoPlayerActivity : BaseActivity<ActivityPhoneVideoPlayerBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
 
     override fun onBackPressed() {
